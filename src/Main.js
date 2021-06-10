f20a4a785bf2947677d9fcc0eadebd105f6079a7
Jacob Peterson <Petersoj@users.noreply.github.com>
2021-06-10 13:01:25 -0600

    Add blurring background_space, update links, add more info

--- src/Main.js
+++ src/Main.js
@@ -3,18 +3,39 @@ import Clipboard from 'clipboard';
 
 new Clipboard('.email-button');
 
-const MobileDetect = require('mobile-detect/mobile-detect.min');
+// Source of below: https://stackoverflow.com/a/20082518
+// Generic function to set blur radius of $ele
+const setBlur = function (ele, radius) {
+        $(ele).css({
+            "-webkit-filter": "blur(" + radius + "px)",
+            "filter": "blur(" + radius + "px)"
+        });
+    },
+    // Generic function to tween blur radius
+    tweenBlur = function (ele, startRadius, endRadius, durationMillis) {
+        $({blurRadius: startRadius}).animate({blurRadius: endRadius}, {
+            duration: durationMillis,
+            easing: 'swing',
+            step: function () {
+                setBlur(ele, this.blurRadius);
+            },
+            callback: function () {
+                setBlur(ele, endRadius);
+            }
+        });
+    };
 
 $(window).on('load', function () {
-    if (MobileDetect.mobile != null) {
-        $('.email-button').attr("href", "mailto:contact@ucubesat.org");
-    } else {
-        $('.email-button').click(function () {
-            const animationClassName = 'animation-up';
-            $('.email-button-copied-text').addClass(animationClassName);
-            setTimeout(function () {
-                $('.email-button-copied-text').removeClass(animationClassName);
-            }, 2000);
-        });
-    }
+    $('.email-button').click(function () {
+        const animationClassName = 'animation-up';
+        $('.email-button-copied-text').addClass(animationClassName);
+        setTimeout(function () {
+            $('.email-button-copied-text').removeClass(animationClassName);
+        }, 2000);
+    });
+
+    setTimeout(function () {
+        $('.content').animate({opacity: 1}, 1000);
+    }, 500);
+    tweenBlur('.landing-background', 0, 10, 1500);
 });
